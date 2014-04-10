92f349f34187e112307c7ce3096ee4acd98c5068
fractaldragon <dragon198658@gmail.com>
2014-04-09 21:56:08 -0500

    some css changes

--- SoReWa/urls.py
+++ SoReWa/urls.py
@@ -3,6 +3,9 @@ from SoReWaApp.views import table
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -13,5 +16,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^table', table)
 
-)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)#+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
