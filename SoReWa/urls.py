f9693f28fa2a1df05c817202d8a496d103a80ad8
fractaldragon <dragon198658@gmail.com>
2014-04-10 22:05:14 -0500

    loads products from category in table page

--- SoReWa/urls.py
+++ SoReWa/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from SoReWaApp.views import table
+from SoReWaApp.views import table, get_products_from_category
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -14,7 +14,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^table', table)
+    url(r'^table/$', table),
+    url(r'^table/category_products/(\w+)/$', get_products_from_category),
 
 )#+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- SoReWaApp/views.py
+++ SoReWaApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
-from SoReWaApp.models import Category
+from SoReWaApp.models import Category, Product
 # Create your views here.
 
 
@@ -50,3 +50,33 @@ def table(request):
     else:
         #show categories and products
         return render(request, 'table.html', {'category_list': category})
+
+
+def get_products_from_category(request, offset):
+    """try:
+
+        if 'categoryName' in request.GET:
+            print("hay nombre")
+            if request.GET['categoryName']:
+                category_name = request.GET['categoryName']
+                print request.GET['categoryName']
+                category = Category.objects.get(name=category_name)
+    except Category.DoesNotExist:
+        print "No Categories in the database yet."
+        return render(request, 'table.html')
+
+    else:
+        return render(request, {'products.html':category.products} )
+            """
+
+    try:
+        offset = str(offset)
+        print offset
+        category = Category.objects.get(name=offset)
+        print category.products.all()
+        return render(request,'products.html', {'products_list': category.products.all()})
+    except Category.DoesNotExist:
+        print "No Categories in the database yet."
+        raise Http404()
+
+
